a98aa2356b66ff7e6296a0970856371c56d13158
Sayan9615 <andreighiorghe06@gmail.com>
2026-04-19 01:40:30 +0300

    permissions finished

--- Program_source/permissions/OthersPermission.cpp
+++ Program_source/permissions/OthersPermission.cpp
@@ -1,1 +1,56 @@
 #include "OthersPermission.h"
+#include "../exceptions/PermissionException.h"
+#include <sstream>
+
+OthersPermission::OthersPermission(bool canRead, bool canWrite)
+:Permission(canRead,canWrite)
+{
+}
+
+OthersPermission::OthersPermission(const OthersPermission &other)
+:Permission(other)
+{
+}
+
+OthersPermission::OthersPermission(OthersPermission &&other) noexcept
+:Permission(std::move(other))
+{
+}
+
+OthersPermission &OthersPermission::operator=(const OthersPermission &other)
+{
+    if(this!=&other)
+    {
+        Permission::operator=(other);
+    }
+    return *this;
+}
+
+OthersPermission &OthersPermission::operator=(OthersPermission &&other) noexcept
+{
+    if(this!=&other)
+    {
+        Permission::operator=(std::move(other));
+    }
+    return *this;
+}
+
+bool OthersPermission::check(const std::string &username,const std::string &operation) const
+{
+   if(operation=="read") 
+        return this->m_canRead;
+   if(operation=="write") 
+        return this->m_canWrite;   
+   
+    throw PermissionException("Operatie necunoscuta: "+operation);
+}
+
+std::string OthersPermission::serialize() const
+{
+    return Permission::serialize();
+}
+
+void OthersPermission::deserialize(const std::string &data)
+{
+      Permission::deserialize(data);
+}
--- Program_source/permissions/OwnerPermission.h
+++ Program_source/permissions/OwnerPermission.h
@@ -3,5 +3,24 @@
 
 class OwnerPermission : public Permission
 {
-    
+    private:
+        std::string m_ownerUsername;
+
+    public:
+        OwnerPermission(const std::string& ownerUsername,bool canRead=true,bool canWrite=true);
+        OwnerPermission(const OwnerPermission &other);
+        OwnerPermission(OwnerPermission&& other) noexcept;
+        
+        OwnerPermission& operator=(const OwnerPermission& other);
+        OwnerPermission& operator=(OwnerPermission&& other)noexcept;
+
+        ~OwnerPermission()=default;
+
+        std::string getOwnerUsername()const {return this->m_ownerUsername;}
+
+        bool check(const std::string& username,const std::string& operation)const override;
+        std::string getType()const override {return "OWNER";}
+
+        std::string serialize()const override;
+        void deserialize(const std::string& data)override;
 };
