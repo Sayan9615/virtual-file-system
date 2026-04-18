76da38af27f2bcbf079c469768758e7e9e1ea2e4
Parastie <127695645+Parastie1@users.noreply.github.com>
2026-04-18 11:17:52 +0300

    Am adaugat membrii claselor, pentru users

--- Program_source/services/AuthService.h
+++ Program_source/services/AuthService.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// de inclus o functie de criptare care sa cripteza parola inainte de stocarea 
+// parolelor, si in baza de date se verifica de asemenea hash-ul parolei
+
 class AuthService
 {
     
