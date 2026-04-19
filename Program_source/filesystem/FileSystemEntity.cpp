04264cf8d3ec6ac57bf5c48c8a5ca333979bfe76
Parastie <mariusparastie@gmail.com>
2026-04-19 13:02:39 +0300

    Am legat Permisions de FileEntity si Am testat logare + share + permisiuni

--- Program_source/filesystem/FileSystemEntity.cpp
+++ Program_source/filesystem/FileSystemEntity.cpp
@@ -1,130 +1,141 @@
 #include "FileSystemEntity.h"
+#include "../services/EventLog.h"
 #include <iostream>
 #include <sstream>
 #include "../exceptions/FileSystemException.h"
 
+FileSystemEntity::FileSystemEntity(const std::string& name,
+                                   const std::string& ownerUser,
+                                   const std::string& ownerGroup,
+                                   iLogger* logger)
+    : m_name(name), m_ownerUser(ownerUser), m_ownerGroup(ownerGroup),
+      m_createdAt(std::time(nullptr)), m_modifiedAt(std::time(nullptr)),
+      m_logger(logger) {
 
-FileSystemEntity::FileSystemEntity(const std::string &name, const std::string &ownerUser, const std::string &ownerGroup)
-:m_name(name),m_ownerUser(ownerUser),m_ownerGroup(ownerGroup),m_createdAt(std::time(nullptr)),m_modifiedAt(std::time(nullptr))
-{
-    if(name.empty())
-        throw FileSystemException("Numele entitatii nu poate gol!");
+    m_permissions.addPermission(
+        std::make_shared<OwnerPermission>(ownerUser, true, true)
+    );
+    m_permissions.addPermission(
+        std::make_shared<GroupPermission>(ownerGroup, true, false)
+    );
+
+    if (m_logger)
+        EventLog(EventType::FILE_UPLOAD, ownerUser, "Entitate creata: " + name, *m_logger);
 }
 
-FileSystemEntity::FileSystemEntity(const FileSystemEntity &other)
-:m_name(other.m_name),m_ownerUser(other.m_ownerUser),m_ownerGroup(other.m_ownerGroup),m_createdAt(other.m_createdAt),m_modifiedAt(other.m_modifiedAt)
-{
-}
+// constructori de copiere/move — adauga m_logger
+FileSystemEntity::FileSystemEntity(const FileSystemEntity& other)
+    : m_name(other.m_name), m_ownerUser(other.m_ownerUser),
+      m_ownerGroup(other.m_ownerGroup), m_createdAt(other.m_createdAt),
+      m_modifiedAt(other.m_modifiedAt), m_permissions(other.m_permissions),
+      m_logger(other.m_logger) {}
 
-FileSystemEntity::FileSystemEntity(FileSystemEntity &&other) noexcept
-:m_name(std::move(other.m_name)),m_ownerUser(std::move(other.m_ownerUser)),m_ownerGroup(std::move(other.m_ownerGroup)),m_createdAt(std::move(other.m_createdAt)),m_modifiedAt(std::move(other.m_modifiedAt))
-{
-}
+FileSystemEntity::FileSystemEntity(FileSystemEntity&& other) noexcept
+    : m_name(std::move(other.m_name)), m_ownerUser(std::move(other.m_ownerUser)),
+      m_ownerGroup(std::move(other.m_ownerGroup)), m_createdAt(other.m_createdAt),
+      m_modifiedAt(other.m_modifiedAt), m_permissions(std::move(other.m_permissions)),
+      m_logger(other.m_logger) {}
 
-FileSystemEntity &FileSystemEntity::operator=(const FileSystemEntity &other)
-{
-    if(this!=&other)
-    {
-        this->m_name=other.m_name;
-        this->m_ownerUser=other.m_ownerUser;
-        this->m_ownerGroup=other.m_ownerGroup;
-        this->m_createdAt=other.m_createdAt;
-        this->m_modifiedAt=other.m_modifiedAt;
-
+FileSystemEntity& FileSystemEntity::operator=(const FileSystemEntity& other) {
+    if (this != &other) {
+        m_name = other.m_name;
+        m_ownerUser = other.m_ownerUser;
+        m_ownerGroup = other.m_ownerGroup;
+        m_createdAt = other.m_createdAt;
+        m_modifiedAt = other.m_modifiedAt;
+        m_permissions = other.m_permissions;
+        m_logger = other.m_logger;
     }
-
     return *this;
 }
 
-FileSystemEntity &FileSystemEntity::operator=( FileSystemEntity &&other) noexcept
-{
-    if(this!=&other)
-    {
-        this->m_name=std::move(other.m_name);
-        this->m_ownerUser=std::move(other.m_ownerUser);
-        this->m_ownerGroup=std::move(other.m_ownerGroup);
-        this->m_createdAt=other.m_createdAt;
-        this->m_modifiedAt=other.m_modifiedAt;
-
+FileSystemEntity& FileSystemEntity::operator=(FileSystemEntity&& other) noexcept {
+    if (this != &other) {
+        m_name = std::move(other.m_name);
+        m_ownerUser = std::move(other.m_ownerUser);
+        m_ownerGroup = std::move(other.m_ownerGroup);
+        m_createdAt = other.m_createdAt;
+        m_modifiedAt = other.m_modifiedAt;
+        m_permissions = std::move(other.m_permissions);
+        m_logger = other.m_logger;
     }
-
     return *this;
 }
 
-void FileSystemEntity::setName(const std::string &name)
-{
-    if(name.empty())
+void FileSystemEntity::setName(const std::string& name) {
+    if (name.empty())
         throw FileSystemException("Numele nu poate fi gol!");
 
-    this->m_name=name;
-    this->m_modifiedAt=std::time(nullptr);    
+    if (m_logger)
+        EventLog(EventType::FILE_UPLOAD, m_ownerUser, 
+                 "Redenumit: " + m_name + " -> " + name, *m_logger);
+
+    m_name = name;
+    m_modifiedAt = std::time(nullptr);
 }
 
-void FileSystemEntity::setOwnerUser(const std::string &user)
-{
-    this->m_ownerUser=user;
+void FileSystemEntity::setOwnerUser(const std::string& user) {
+    if (m_logger)
+        EventLog(EventType::FILE_SHARE, m_ownerUser,
+                 "Owner schimbat: " + m_name + " -> " + user, *m_logger);
+    m_ownerUser = user;
 }
 
-void FileSystemEntity::setOwnerGroup(const std::string &group)
-{
-    this->m_ownerGroup=group;
+void FileSystemEntity::setOwnerGroup(const std::string& group) {
+    if (m_logger)
+        EventLog(EventType::FILE_SHARE, m_ownerUser,
+                 "Grup schimbat: " + m_name + " -> " + group, *m_logger);
+    m_ownerGroup = group;
 }
 
-void FileSystemEntity::display() const
-{
-    std::cout<<getIcon()<<" "<<this->m_name<<" | owner: "<<this->m_ownerUser<<" |group: "<<this->m_ownerGroup<<" | size: "<<this->getSize()<<" bytes"<<std::endl;
+// restul metodelor raman identice
+void FileSystemEntity::display() const {
+    std::cout << getIcon() << " " << m_name << " | owner: " << m_ownerUser
+              << " | group: " << m_ownerGroup << " | size: " << getSize() << " bytes\n";
 }
 
-std::string FileSystemEntity::getIcon() const
-{
-    return this->isFolder()?"[DIR]":"[FILE]";
+std::string FileSystemEntity::getIcon() const {
+    return isFolder() ? "[DIR]" : "[FILE]";
 }
 
-std::string FileSystemEntity::serialize() const
-{
+std::string FileSystemEntity::serialize() const {
     std::ostringstream oss;
-    oss<<this->m_name<<"|"<<this->m_ownerUser<<"|"<<this->m_ownerGroup<<"|"<<this->m_createdAt<<"|"<<this->m_modifiedAt;
+    oss << m_name << "|" << m_ownerUser << "|" << m_ownerGroup
+        << "|" << m_createdAt << "|" << m_modifiedAt;
     return oss.str();
 }
 
-void FileSystemEntity::deserialize(const std::string &data)
-{
+void FileSystemEntity::deserialize(const std::string& data) {
     std::istringstream iss(data);
     std::string token;
     std::vector<std::string> tokens;
 
-    while(std::getline(iss,token,'|'))
-    {
+    while (std::getline(iss, token, '|'))
         tokens.push_back(token);
-    }
 
-    if(tokens.size()<5)
+    if (tokens.size() < 5)
         throw FileSystemException("Date invalide pentru deserializare entitate!");
 
-    this->m_name=tokens[0];
-    this->m_ownerUser=tokens[1];
-    this->m_ownerGroup=tokens[2];
-    this->m_createdAt=std::stoll(tokens[3]);
-    this->m_modifiedAt=std::stoll(tokens[4]);
+    m_name = tokens[0];
+    m_ownerUser = tokens[1];
+    m_ownerGroup = tokens[2];
+    m_createdAt = std::stoll(tokens[3]);
+    m_modifiedAt = std::stoll(tokens[4]);
 }
 
-bool FileSystemEntity::operator==(const FileSystemEntity &other) const
-{
-    return this->m_name == other.m_name && this->m_ownerGroup==other.m_ownerGroup;
+bool FileSystemEntity::operator==(const FileSystemEntity& other) const {
+    return m_name == other.m_name && m_ownerGroup == other.m_ownerGroup;
 }
 
-bool FileSystemEntity::operator!=(const FileSystemEntity &other) const
-{
-    return !(*this==other);
+bool FileSystemEntity::operator!=(const FileSystemEntity& other) const {
+    return !(*this == other);
 }
 
-bool FileSystemEntity::operator<(const FileSystemEntity &other) const
-{
-    return this->m_name<other.m_name;
+bool FileSystemEntity::operator<(const FileSystemEntity& other) const {
+    return m_name < other.m_name;
 }
 
-std::ostream &operator<<(std::ostream &os, const FileSystemEntity &entity)
-{
-    os<< entity.getIcon()<<" "<<entity.m_name<<" ("<<entity.m_ownerUser<<")";
+std::ostream& operator<<(std::ostream& os, const FileSystemEntity& entity) {
+    os << entity.getIcon() << " " << entity.m_name << " (" << entity.m_ownerUser << ")";
     return os;
 }
