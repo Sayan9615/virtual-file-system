a96fc27d78dda00b8bae2dc085ac87e1e23a5c9c
Parastie <mariusparastie@gmail.com>
2026-04-20 17:47:31 +0300

    Am modificat gruop permision, am facut un main prietenos, si am implementat search

--- build/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ATMosFILE_FILE C:/Users/paras/Documents/GitHub/virtual-file-system/build/ATMosFILE.exe)
-set(__QT_DEPLOY_TARGET_ATMosFILE_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ATMosFILE_RUNTIME_DLLS C:/Qt/6.11.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/msvc2022_64/bin/Qt6Core.dll)
